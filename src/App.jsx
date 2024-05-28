cb490e7e3a54574f69fce064996f72e1ab9797ab
joewilson04 <joebot24@outlook.com>
2024-05-28 11:43:53 +0100

    removed EXAMPLE and tabButton import from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Fragment } from "react";
 import { Header } from "./components/header/Header.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { EXAMPLES } from "./data.js";
 import CoreConcepts1 from "./components/CoreConcept.jsx";
 import Examples from "./components/Examples.jsx";
 
